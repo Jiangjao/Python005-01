882d8db8c3c72e10090071f2c5cbdf601b8abfb6
jayjiao <918718278@qq.com>
2021-01-17 20:02:39 +0800

    modified

--- week07/homework2.py
+++ week07/homework2.py
@@ -1,26 +1,15 @@
-def outer_arg(bar):
-    def outer(func):
-        def inner(*args,**kwargs):
-            # 判断bar的类型
-            if isinstance(bar,dict):
-                print(' watch out a dict  ')
-                try:
-                    for index in range(len(bar)):
-                        ret = func(bar[index])
-                except Exception as error:
-                    print(error)
-                    
-                # return 
-            for index in range(len(bar)):
-                ret = func(bar[index])
-            print(bar)
-            return ret
-        return inner
-    return outer
-
-@outer_arg({'1':1,'2':2})
 def sayHello(index=1):
     print(f'Hello Wolrd {index}')
-    
+
+def my_map(fun,*iterations):
+    m = len(iterables)
+    try:
+        n = len(iterables[0])
+    except TypeError as e:
+        raise TypeError('please input iterables')
+    for items in zip(*iterations):
+	    yield fun(*items)
+
 # sayHello(1,2,3,4)
-sayHello(1)
+
+my_map(sayHello,{'1':1,'2':2})
--- week07/homework3.py
+++ week07/homework3.py
@@ -2,13 +2,13 @@ import time
 import math
 
 def timer(func):
-    def inner():
+    def inner(*args, **kwargs):
         start = time.time()
-        func()
+        func(*args, **kwargs)
         end = time.time()
         time_delata =  end - start
         # time_delata = round((end - start),10)
-        print(f"花费时间大概{time_delata}毫秒")
+        print(func.__name__,f"花费时间大概{time_delata}毫秒")
         return time_delata
     return inner
 @timer
